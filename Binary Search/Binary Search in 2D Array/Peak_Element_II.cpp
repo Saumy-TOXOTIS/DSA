145da73fe304f91db2652a15286e0248b735e4bf
Saumy-TOXOTIS <saumytiwari95@gmail.com>
2025-09-19 18:27:04 +0530

    Binary Search

--- Binary Search/Binary Search in 2D Array/Peak_Element_II.cpp
+++ Binary Search/Binary Search in 2D Array/Peak_Element_II.cpp
@@ -5,5 +5,224 @@ vector<int> findPeakGrid(vector<vector<int>>& mat)
 {
     int n = mat.size();
     int m = mat[0].size();
-    
+    int low = 0;
+    int high = m - 1;
+    while(low <= high)
+    {
+        int mid = low + (high - low)/2;
+        bool shrinkLeft = false;
+        for(int i = 0;i < n;i++)
+        {
+            if(i == 0 && i == n - 1)
+            {
+                if(mid == 0 && mid == m - 1)
+                {
+                    return {i,mid};
+                }
+                else if(mid == 0)
+                {
+                    if(mat[i][mid] > mat[i][mid + 1])
+                    {
+                        return {i,mid};
+                    }
+                    else
+                    {
+                        shrinkLeft = true;
+                        break;
+                    }
+                }
+                else if(mid == m - 1)
+                {
+                    if(mat[i][mid] > mat[i][mid - 1])
+                    {
+                        return {i,mid};
+                    }
+                    else
+                    {
+                        shrinkLeft = false;
+                        break;
+                    }
+                }
+                else
+                {
+                    if(mat[i][mid] > mat[i][mid - 1] && mat[i][mid] > mat[i][mid + 1])
+                    {
+                        return {i,mid};
+                    }
+                    else if(mat[i][mid - 1] > mat[i][mid])
+                    {
+                        shrinkLeft = false;
+                        break;
+                    }
+                    else
+                    {
+                        shrinkLeft = true;
+                        break;
+                    }
+                }
+            }
+            else if(i == 0)
+            {
+                if(mat[i][mid] > mat[i + 1][mid])
+                {
+                    if(mid == 0 && mid == m - 1)
+                    {
+                        return {i,mid};
+                    }
+                    else if(mid == 0)
+                    {
+                        if(mat[i][mid] > mat[i][mid + 1])
+                        {
+                            return {i,mid};
+                        }
+                        else
+                        {
+                            shrinkLeft = true;
+                            break;
+                        }
+                    }
+                    else if(mid == m - 1)
+                    {
+                        if(mat[i][mid] > mat[i][mid - 1])
+                        {
+                            return {i,mid};
+                        }
+                        else
+                        {
+                            shrinkLeft = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if(mat[i][mid] > mat[i][mid - 1] && mat[i][mid] > mat[i][mid + 1])
+                        {
+                            return {i,mid};
+                        }
+                        else if(mat[i][mid - 1] > mat[i][mid])
+                        {
+                            shrinkLeft = false;
+                            break;
+                        }
+                        else
+                        {
+                            shrinkLeft = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            else if(i == n - 1)
+            {
+                if(mat[i][mid] > mat[i - 1][mid])
+                {
+                    if(mid == 0 && mid == m - 1)
+                    {
+                        return {i,mid};
+                    }
+                    else if(mid == 0)
+                    {
+                        if(mat[i][mid] > mat[i][mid + 1])
+                        {
+                            return {i,mid};
+                        }
+                        else
+                        {
+                            shrinkLeft = true;
+                            break;
+                        }
+                    }
+                    else if(mid == m - 1)
+                    {
+                        if(mat[i][mid] > mat[i][mid - 1])
+                        {
+                            return {i, mid};
+                        }
+                        else
+                        {
+                            shrinkLeft = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if(mat[i][mid] > mat[i][mid - 1] && mat[i][mid] > mat[i][mid + 1])
+                        {
+                            return {i,mid};
+                        }
+                        else if(mat[i][mid - 1] > mat[i][mid])
+                        {
+                            shrinkLeft = false;
+                            break;
+                        }
+                        else
+                        {
+                            shrinkLeft = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                if(mat[i][mid] > mat[i - 1][mid] && mat[i][mid] > mat[i + 1][mid])
+                {
+                    if(mid == 0 && mid == m - 1)
+                    {
+                        return {i,mid};
+                    }
+                    else if(mid == 0)
+                    {
+                        if(mat[i][mid] > mat[i][mid + 1])
+                        {
+                            return {i,mid};
+                        }
+                        else
+                        {
+                            shrinkLeft = true;
+                            break;
+                        }
+                    }
+                    else if(mid == m - 1)
+                    {
+                        if(mat[i][mid] > mat[i][mid - 1])
+                        {
+                            return {i,mid};
+                        }
+                        else
+                        {
+                            shrinkLeft = false;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        if(mat[i][mid] > mat[i][mid - 1] && mat[i][mid] > mat[i][mid + 1])
+                        {
+                            return {i,mid};
+                        }
+                        else if(mat[i][mid - 1] > mat[i][mid])
+                        {
+                            shrinkLeft = false;
+                            break;
+                        }
+                        else
+                        {
+                            shrinkLeft = true;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        if(shrinkLeft)
+        {
+            low = mid + 1;
+        }
+        else
+        {
+            high = mid - 1;
+        }
+    }
+    return {-1,-1};
 }
